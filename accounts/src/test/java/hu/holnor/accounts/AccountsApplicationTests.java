4e25ae74015d89cf1e70fe2e8a1ac3876dd092b4
holczur <holcz.norbert@gmail.com>
2023-10-10 08:16:47 +0200

    project init

---
+++ accounts/src/test/java/hu/holnor/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.holnor.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
