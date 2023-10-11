45b858328f885f0735d93eb96def44c293f4e4b9
holczur <holcz.norbert@gmail.com>
2023-10-11 12:54:16 +0200

    create Controller layer for Account

--- accounts/src/main/java/hu/holnor/accounts/controller/AccountsController.java
+++ accounts/src/main/java/hu/holnor/accounts/controller/AccountsController.java
@@ -1,13 +1,37 @@
 package hu.holnor.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.holnor.accounts.constants.AccountConstants;
+import hu.holnor.accounts.dto.CustomerDto;
+import hu.holnor.accounts.dto.ResponseDto;
+import hu.holnor.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/api", produces={MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
 
-    @GetMapping("sayHello")
-    public String sayHello(){
-        return "Hi World";
+    private IAccountService iAccountService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        iAccountService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
+        CustomerDto customerDto = iAccountService.fetchAccountDetails(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(customerDto);
+    }
+
 }
