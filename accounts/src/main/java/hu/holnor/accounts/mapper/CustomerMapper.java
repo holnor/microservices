d05b5f2e25cbf469219fd4a9c6199c43ccacda7b
holczur <holcz.norbert@gmail.com>
2023-10-11 12:53:51 +0200

    create CustomerMapper

---
+++ accounts/src/main/java/hu/holnor/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,23 @@
+package hu.holnor.accounts.mapper;
+
+import hu.holnor.accounts.dto.CustomerDto;
+import hu.holnor.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+
+}
