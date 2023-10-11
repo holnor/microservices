d9a16a563dfd1e9a6c7ab6e9d853f3d0aae7409f
holczur <holcz.norbert@gmail.com>
2023-10-11 12:53:23 +0200

    create repository layer

---
+++ accounts/src/main/java/hu/holnor/accounts/repository/AccountRepository.java
@@ -0,0 +1,15 @@
+package hu.holnor.accounts.repository;
+
+import hu.holnor.accounts.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findAccountByCustomerId(Long customerId);
+
+}
+
---
+++ accounts/src/main/java/hu/holnor/accounts/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package hu.holnor.accounts.repository;
+
+import hu.holnor.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+}
