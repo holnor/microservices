3f94d390f6d0d9927b52d75dad1ab680f6c1b281
holczur <holcz.norbert@gmail.com>
2023-10-11 12:52:30 +0200

    introduce new DTO for response (code&msg)

---
+++ accounts/src/main/java/hu/holnor/accounts/dto/ResponseDto.java
@@ -0,0 +1,13 @@
+package hu.holnor.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String statusCode;
+    private String statusMsg;
+
+}
